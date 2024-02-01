e5837088e04dc811f4b67d7b3bcc4223d59457df
hudson <hfinn@autodist.com>
2024-02-01 11:55:25 -0500

    day 19

---
+++ src/2022/day_19/solution.rs
@@ -0,0 +1,14 @@
+struct Blueprint {}
+
+struct Pack {
+    ore_robot: u32,
+    clay_robot: u32,
+    obsidian_robot: u32,
+    geode_robot: u32,
+    ore: u32,
+    clay: u32,
+    obsidian: u32,
+    geode: u32,
+}
+
+pub fn best_blueprint() {}
